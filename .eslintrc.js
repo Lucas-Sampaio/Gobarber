5f824fe24f53e349b05049347466437b2c3f47e7
lucas.souza <lucas.souza@opensystem.srv.br>
2021-02-28 21:45:46 -0300

    corrigido config do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,35 @@
 module.exports = {
   env: {
     es2021: true,
+
     node: true,
   },
-  extends: ["airbnb-base", "prettier", "plugin:node/recommended"],
-  plugins: ["prettier"],
+
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+
+  plugins: ['prettier'],
+
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+
+    SharedArrayBuffer: 'readonly',
   },
+
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+
+    sourceType: 'module',
   },
+
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    camelcase: "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    'prettier/prettier': 'error',
+
+    'class-methods-use-this': 'off',
+
+    'no-param-reassign': 'off',
+
+    camelcase: 'off',
+
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
