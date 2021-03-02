27e992ff6cee38594335be9aa5a5d8dc5ca5175d
lucas.souza <lucas.souza@opensystem.srv.br>
2021-03-02 00:30:01 -0300

    identado codigo

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,42 +4,40 @@ import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
-class SessionController{
-
-  async store(req,res){
-
+class SessionController {
+  async store(req, res) {
     try {
       const schema = Yup.object().shape({
         email: Yup.string().email().required(),
-        password:Yup.string().required()
+        password: Yup.string().required(),
       });
 
-      if(!(await schema.isValid(req.body))){
-        return res.status(400).json({error:'validation fails'});
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'validation fails' });
       }
 
-      const {email,password} = req.body;
-      const user = await User.findOne({where:{email}});
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
 
-      if(!user){
-        return res.status(401).json({erro:'User not found'});
+      if (!user) {
+        return res.status(401).json({ erro: 'User not found' });
       }
 
-      if(!(await user.checkPassword(password))){
-        return res.status(401).json({error:'Password does not match'});
+      if (!(await user.checkPassword(password))) {
+        return res.status(401).json({ error: 'Password does not match' });
       }
 
-      const {id,name} = user;
-      let userResponse = {id,name,email};
-      let token = jwt.sign({id},authConfig.secret,{expiresIn:authConfig.expiresIn});
-      let resposta = {userResponse,token}
+      const { id, name } = user;
+      const userResponse = { id, name, email };
+      const token = jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      });
+      const resposta = { userResponse, token };
       return res.json(resposta);
-
     } catch (error) {
       return res.json(error);
     }
   }
-
 }
 
 export default new SessionController();
--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken';
-import {promisify} from 'util';
+import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
-export default async (req,res,next) => {
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if(!authHeader){
-    return res.status(401).json({error:'Token not provided'});
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
-  const [,token] = authHeader.split(' ');
+  const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = await promisify(jwt.verify)(token,authConfig.secret);
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     req.userId = decoded.id;
-    console.log(decoded);
+
     return next();
   } catch (error) {
-    return res.status(401).json({error:'Token invalid'});
+    return res.status(401).json({ error: 'Token invalid' });
   }
-}
+};
