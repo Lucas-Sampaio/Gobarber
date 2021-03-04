4438a9cf12199c1aa2139141dbe217597bc0ac5b
lucas.souza <lucas.souza@opensystem.srv.br>
2021-03-03 23:38:09 -0300

    adicionado envio de email com template

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '745256d89dfe8d',
+    pass: '6d3cbda6301874',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
